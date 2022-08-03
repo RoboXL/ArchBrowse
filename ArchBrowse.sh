4dc7d39ac4b184900eb02c30ad56db71c923337d
RoboXL <105053104+RoboXL@users.noreply.github.com>
2022-08-03 18:22:42 -0400

    Create ArchBrowse.sh

---
+++ ArchBrowse.sh
@@ -0,0 +1,22 @@
+echo "#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#
+#Warning: I recommend that you update/upgrade arch before doing this#
+#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#"
+echo "                                                                       
+                                                                                         "
+echo "You can use this https://github.com/RoboXL/Arch-Updater or https://github.com/RoboXL/Arch-Updater-Yay"
+echo "                                                                       
+                                                                                         "
+echo "###############################################################################
+#(You need to have yay installed or you can make another version and share it)#
+#                                                                             #
+# Welcome Please select your browser. After that you may close the window :)  #
+###############################################################################"
+select yn in "Firefox" "Chrome" "Brave" "Chromium" "Edge"; do
+    case $yn in
+        Firefox ) echo | sudo pacman -S firefox;;
+        Chrome ) echo | yay -S google-chrome;;
+        Brave ) echo | yay -S brave-bin;;
+        Chromium ) echo | sudo pacman -S chromium;;
+        Edge ) echo | yay -S microsoft-edge-stable-bin;;
+    esac
+done
